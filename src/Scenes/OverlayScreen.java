63266ca6fb5292fa7a2d8f6a8f2a23a5283e9b6e
mannu kumar <mannukumar233@gmail.com>
2025-12-30 00:51:39 +0530

    added a overlay screen  for utility like health bar , lives ,etc

--- src/Scenes/OverlayScreen.java
+++ src/Scenes/OverlayScreen.java
@@ -1,9 +1,11 @@
 package Scenes;
 
 import javax.swing.*;
+import java.awt.*;
 
 
 public class OverlayScreen extends JLayeredPane {
+
 
 
     public static OverlayScreen overlayScreen;
@@ -14,5 +16,13 @@ public class OverlayScreen extends JLayeredPane {
             overlayScreen=new OverlayScreen();
         }
         return overlayScreen;
+    }
+
+
+    public void draw(Graphics g){
+        super.paintComponent(g);
+        g.setColor(Color.red);
+        g.fillRect(GamePanel.WIDTH-100,GamePanel.HEIGHT,80,20);
+
     }
 }
