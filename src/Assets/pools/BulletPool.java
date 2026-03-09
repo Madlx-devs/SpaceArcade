1c1233edcb0b5265474f6e388efea90e0709a1c1
mannu kumar <mannukumar233@gmail.com>
2026-03-10 00:15:43 +0530

    modify:bullet pool

--- src/Assets/pools/BulletPool.java
+++ src/Assets/pools/BulletPool.java
@@ -6,14 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BulletPool implements ObjectPool<Bullet> {
+ private static  BulletPool bulletPool =null;
  private final  List<Bullet> available= new ArrayList<>();
  private final List<Bullet> inUse = new ArrayList<>();
 
-    public BulletPool(int initialSize) {
+    private BulletPool(int initialSize) {
         for (int i = 0; i < initialSize; i++) {
             available.add(new Bullet());
         }
     }
+
+    public static BulletPool getInstance() {
+        if(bulletPool==null){
+           bulletPool= new BulletPool(10);
+        }
+        return bulletPool;
+    }
+
     @Override
     public Bullet acquireObject() {
         return available.isEmpty() ? new Bullet() : available.remove(available.size() - 1);
